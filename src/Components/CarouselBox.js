3ac994293c245ef409bd992f01614c5848bc642d
Yevhen-Yanushevych <enotamslava@gmail.com>
2021-02-15 00:40:30 +0200

    d

--- src/Components/CarouselBox.js
+++ src/Components/CarouselBox.js
@@ -13,6 +13,18 @@ export default class CarouselBox extends Component{
                 <Carousel.Item>
                     <img
                     className="d-block w-100"
+                    src={Forest}
+                    alt="Swim"
+                    
+                    />
+                    <Carousel.Caption>
+                        <h3>Taiwan</h3>
+                        <p>is a U.S. state on the northwest extremity of the country's West Coast, just across the Bering Strait.is a country in East Asia. Neighbouring countries include the People's Republic of China (PRC) to the northwest, Japan to the northeast, and the Philippines to the south.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
                     src={Iceberg}
                     alt="Swim"
                     
@@ -20,18 +32,6 @@ export default class CarouselBox extends Component{
                     <Carousel.Caption>
                         <h3>Alaska</h3>
                         <p>is a U.S. state on the northwest extremity of the country's West Coast, just across the Bering Strait.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={Forest}
-                    alt="Swim"
-                    
-                    />
-                    <Carousel.Caption>
-                        <h3>Taiwan</h3>
-                        <p>is a country in East Asia. Neighbouring countries include the People's Republic of China (PRC) to the northwest, Japan to the northeast, and the Philippines to the south.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
